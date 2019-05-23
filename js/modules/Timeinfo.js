00fbbcc8a6d02689038ff847b74f2cf38a0acded
Malgorzata Lesniak <M999@interia.eu>
2019-05-24 00:25:28 +0200

    Add modules

---
+++ js/modules/Timeinfo.js
@@ -0,0 +1,13 @@
+var os = require('os');
+
+
+var setTimeinfo = function () {
+    var time = os.uptime;
+    var tabTime = [];
+    tabTime[0] = (time /3600).toFixed(0); // ilość godzin
+    tabTime[1] = ((time - tabTime[0]*3600)/60).toFixed(0); // ilość minut po odliczeniu godzin
+    tabTime[2] = time - tabTime[0]*3600 - tabTime[1]*60; // ilość sekund
+    console.log('Uptime: ~',tabTime[0],'godz.' ,tabTime[1] , 'minut', tabTime[1], 'sekund');
+
+ }
+exports.changeTimeinfo = setTimeinfo;
